cebd87563a3f71bb71341e25548d52570a132098
oscar123bech-gif <oscar123bech@gmail.com>
2026-07-20 18:57:42 +0200

    Add files via upload

--- linearregression.py
+++ linearregression.py
@@ -22,3 +22,24 @@ print(predictedy)
 plt.scatter(x,y)
 plt.plot(x,predictedy)
 plt.show()
+
+error = np.sqrt(np.mean((predictedy - y)**2))
+
+print(error)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import root_mean_squared_error
+print(x.shape)
+x = x.reshape(-1,1)
+print(x.shape)
+
+lr = LinearRegression()
+lr.fit(x,y)
+print(lr.coef_)
+print(lr.intercept_)
+predictedy2 = lr.predict(x)
+print(predictedy2)
+
+
+error2 = root_mean_squared_error(y,predictedy2)
+print(error2)
