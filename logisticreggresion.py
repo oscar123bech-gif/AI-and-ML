cbcd552ed42ae0daae2073a9b432df9c8f986d7a
oscar123bech-gif <oscar123bech@gmail.com>
2026-08-30 17:47:09 +0100

    Add files via upload

--- logisticreggresion.py
+++ logisticreggresion.py
@@ -5,8 +5,38 @@ csv = pd.read_csv("titanic.csv")
 csv.info()
 print (csv.notnull().sum())
 
+from sklearn.preprocessing import LabelEncoder
+print(csv["Sex"].value_counts())
+le = LabelEncoder()
+csv["Sex"] = le.fit_transform(csv["Sex"])
+csv.info()
+
 X = csv[["Sex","Fare","Pclass"]]
 y = csv["Survived"]
 
 
-print(csv["Sex"].value_counts())
+
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=1,test_size=0.1)
+from sklearn.linear_model import LogisticRegression
+lg = LogisticRegression()
+
+lg.fit(X_train,y_train)
+
+predictedy = lg.predict(X_test)
+
+print(predictedy,y_test)
+
+from sklearn.metrics import confusion_matrix,classification_report
+
+error = confusion_matrix(y_test,predictedy)
+print(error)
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns 
+sns.heatmap(error,annot=True,fmt="d")
+
+plt.show()
+print(classification_report(y_test,predictedy))
